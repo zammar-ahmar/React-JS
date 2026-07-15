5ea76ca7fccfde85535f1805a9c9ef56e2c067fb
zammar-ahmar <159016048+zammar-ahmar@users.noreply.github.com>
2026-07-15 23:23:26 +0500

    changes

--- reactRouter/src/components/Header/Header.jsx
+++ reactRouter/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
@@ -38,14 +38,16 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                    className={() =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100
-                                         hover:bg-gray-50 lg:hover:bg-transparent lg:border-0
-                                          hover:text-orange-700 lg:p-0`
-                                    }
-                                >
-                                    Home
-                                </NavLink>
+                                  to="/"
+                                  className={({isActive}) =>
+                                    `block py-2 pr-4 pl-3 duration-200 
+                                    ${isActive ? 'bg-orange-700 text-white' : 'hover:bg-gray-50'} 
+                                    border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0
+                                    hover:text-orange-700 lg:p-0`
+    }
+                                    >
+                                        Home
+                                    </NavLink>
                             </li>
                             
                             
