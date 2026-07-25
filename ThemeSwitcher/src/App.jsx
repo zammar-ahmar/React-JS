01776851566f16eeb79979d327d766661b2e7d1c
zammar-ahmar <159016048+zammar-ahmar@users.noreply.github.com>
2026-07-25 11:47:27 +0500

    css issue resolved dark mode

--- ThemeSwitcher/src/App.jsx
+++ ThemeSwitcher/src/App.jsx
@@ -28,7 +28,9 @@ function App() {
         darkMode,
       }}
     >
-      <div className="flex flex-wrap min-h-screen items-center">
+  
+      
+        <div className="flex flex-wrap min-h-screen  items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             <ThemeBtn />
@@ -39,6 +41,7 @@ function App() {
              </div>
         </div>
       </div>
+    
     </ThemeProvider>
   );
 }
